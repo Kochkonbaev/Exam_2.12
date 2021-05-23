b2257fb81dcfb167658ad94bbd3cc1dc3498f6b1
Abdimusa Kochkonbaev <musa301196@gmail.com>
2021-05-23 04:54:54 +0300

    Третье задание

---
+++ task3.py
@@ -0,0 +1,92 @@
+students = [
+    {'name': 'John', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Andrew', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Marcus', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Agness', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Don', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Michael', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Jennifer', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Angela', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Eniston', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Albert', 'age': 33, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nash', 'age': 28, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Nicolas', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Alexey', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Martin', 'age': 22, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Gloria', 'age': 23, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Mikel', 'age': 24, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Susanne', 'age': 25, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Stevie', 'age': 26, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Mark', 'age': 18, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Johnathan', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aidin', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Mirbek', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aidana', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Atay', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Chyngyz', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Aigerim', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Jyldyz', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Emir', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Emirlan', 'age': 12, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nursultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aliaskar', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aktanbek', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Adyl', 'age': 14, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Janyl', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Meerim', 'age': 12, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Sultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+]
+
+courses = {
+    'python':[],
+    'java':[],
+    'javascript':[]
+}
+
+
+for i in students:
+    course_x = i.get('course')
+    name_x = i.get('name')
+    
+    if course_x == 'python':
+        courses['python'].append(name_x)
+
+    elif course_x == 'java':
+        courses['java'].append(name_x)
+    
+    elif course_x == 'javascript':
+        courses['javascript'].append(name_x)
+
+
+# print(courses)
+
+names_ages = {}
+
+for i in students:
+    name_a = i.get('name')
+    age_x = i.get('age')
+
+    names_ages[name_a]=  age_x
+    
+
+# print(names_ages)
+
+
+names = ['Janyl', 'Nursultan', 'Meerim', 'Emir', 'Susann', 'Marcus', 'Aidin', 
+'Aigerim', 'Angela', 'Albert', 'Jyldyz', 'Doe', 'Gloria', 'Aliaskar',
+ 'Martin', 'John', 'Andrew', 'Steve', 'Johnathan', 'Adyl', 'Chyngyz', 
+'Michael', 'Atay', 'Mikkel', 'Agnes', 'Aidana', 'Sultan', 'Nash',
+ 'Nicolas', 'Mirbek', 'Aktan', 'Emirlan', 'Jennifer', 'Eniston', 'Alex', 'Mark']
+
+
+def func(lst, kwargs):
+    for i in lst:
+        try:
+            print(f'{i} --{kwargs[i]}')
+
+        except KeyError:
+            print(f"Такого {i} имени в словаре не существует")
+
+
+func(names, names_ages)
+        
